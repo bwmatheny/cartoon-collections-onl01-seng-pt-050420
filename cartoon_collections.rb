4de41010cffb3dc02c9f5feb291b6c122dd0013a
Brian Matheny <bwmatheny@gmail.com>
2020-05-12 14:35:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|name| puts "\#{name}"}
+  dwarves.each_with_index {|name| puts "\#{index}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
